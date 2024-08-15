aeebea92e89a7ccbea3d4d84023265370b58d3d5
deividas-butkus <deividas.butkus@hotmail.com>
2024-08-15 20:37:14 +0300

    useState for isOpen

--- 04-steps/src/App.jsx
+++ 04-steps/src/App.jsx
@@ -8,6 +8,7 @@ const messages = [
 
 function App() {
   const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
 
   function handlePreviousClick() {
     step > 1 && setStep(step - 1);
@@ -17,31 +18,46 @@ function App() {
     step < 3 && setStep(step + 1);
   }
 
+  function handleClose() {
+    setIsOpen(!isOpen);
+  }
+
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={step >= 1 ? "active" : ""}>1</div>
-        <div className={step >= 2 ? "active" : ""}>2</div>
-        <div className={step >= 3 ? "active" : ""}> 3</div>
-      </div>
-      <p className="message">
-        Step {step}: {messages[step - 1]}
-      </p>
-      <div className="buttons">
-        <button
-          style={step > 1 ? { backgroundColor: "#7950f2", color: "#fff" } : {}}
-          onClick={handlePreviousClick}
-        >
-          Previous
-        </button>
-        <button
-          style={step < 3 ? { backgroundColor: "#7950f2", color: "#fff" } : {}}
-          onClick={handleNextClick}
-        >
-          Next
-        </button>
-      </div>
-    </div>
+    <>
+      <button className="close" onClick={handleClose}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}> 3</div>
+          </div>
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
+          <div className="buttons">
+            <button
+              style={
+                step > 1 ? { backgroundColor: "#7950f2", color: "#fff" } : {}
+              }
+              onClick={handlePreviousClick}
+            >
+              Previous
+            </button>
+            <button
+              style={
+                step < 3 ? { backgroundColor: "#7950f2", color: "#fff" } : {}
+              }
+              onClick={handleNextClick}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
