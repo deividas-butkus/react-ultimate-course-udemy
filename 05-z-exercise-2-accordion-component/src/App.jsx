1f2fd939cdfb829fd3cbe3fdfb7599dbdac64846
deividas-butkus <deividas.butkus@hotmail.com>
2024-08-18 16:35:46 +0300

    05 - exercise #2 - accordion component - redundant title parameter removed from handleToggle fn

--- 05-z-exercise-2-accordion-component/src/App.jsx
+++ 05-z-exercise-2-accordion-component/src/App.jsx
@@ -47,10 +47,7 @@ function AccordionItem({ num, title, text }) {
   };
 
   return (
-    <div
-      className={`item ${isOpen && "open"}`}
-      onClick={() => handleToggle(title)}
-    >
+    <div className={`item ${isOpen && "open"}`} onClick={() => handleToggle()}>
       <p className="number">{num < 10 ? `0${num}` : num}</p>
       <p className="title">{title}</p>
       <p className="icon">{isOpen ? "-" : "+"}</p>
