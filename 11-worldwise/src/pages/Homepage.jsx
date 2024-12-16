e62faa2039ae66a68d9cbbd77cd9e1526478bbdb
deividas-butkus <deividas.butkus@hotmail.com>
2024-12-16 19:57:08 +0200

    11 - WORLDWISE - CSS Modules

--- 11-worldwise/src/pages/Homepage.jsx
+++ 11-worldwise/src/pages/Homepage.jsx
@@ -1,10 +1,15 @@
+import { Link } from "react-router-dom";
 import PageNav from "../components/PageNav";
+import AppNav from "../components/AppNav";
 
 function Homepage() {
   return (
     <div>
       <PageNav />
-      <h1>WordWise</h1>
+      <AppNav />
+      <h1 className="test">WordWise</h1>
+
+      <Link to="/app">Go to the app</Link>
     </div>
   );
 }
