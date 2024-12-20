6f99b3123b4fcb9aa32add673d7585f8634bf89a
deividas-butkus <deividas.butkus@hotmail.com>
2024-12-20 18:28:19 +0200

    11 - WORLDWISE - change json - change props order

--- 11-worldwise/src/components/CityList.jsx
+++ 11-worldwise/src/components/CityList.jsx
@@ -3,13 +3,14 @@ import styles from "./CityList.module.css";
 import Spinner from "./Spinner";
 import Message from "./Message";
 
-function CityList({ isLoading, cities }) {
+function CityList({ cities, isLoading }) {
   if (isLoading) return <Spinner />;
 
   if (!cities.length)
     return (
       <Message message="Add your first city by clicking on a city on the map" />
     );
+
   return (
     <ul className={styles.cityList}>
       {cities.map((city) => (
