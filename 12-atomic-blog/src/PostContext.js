00b5d433445232005e0d86bedc7d5ee22d00c517
deividas-butkus <deividas.butkus@hotmail.com>
2024-12-23 09:15:44 +0200

    12 - ATOMIC_BLOG - import and use createRandomPost() in PostContext

--- 12-atomic-blog/src/PostContext.js
+++ 12-atomic-blog/src/PostContext.js
@@ -1,12 +1,5 @@
 import { createContext, useState } from "react";
-import { faker } from "@faker-js/faker";
-
-function createRandomPost() {
-  return {
-    title: `${faker.hacker.adjective()} ${faker.hacker.noun()}`,
-    body: faker.hacker.phrase(),
-  };
-}
+import createRandomPost from "./utils/createRandomPost";
 
 // 1) Create a Context
 const PostContext = createContext();
