ae0399c8be0dc215b639b3a819fae34c3dec0f47
deividas-butkus <deividas.butkus@hotmail.com>
2024-12-24 15:20:06 +0200

    11 - WORLDWISE - apply useCities in Map and change Marker to cities position

--- 11-worldwise/src/components/Map.jsx
+++ 11-worldwise/src/components/Map.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import styles from "./Map.module.css";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import useCities from "../contexts/useCities";
 
 function Map() {
   const [mapPosition /* setMapPosition */] = useState([40, 0]);
+  const { cities } = useCities();
 
   // const [searchParams /* setSearchParams */] = useSearchParams();
 
@@ -22,11 +24,16 @@ function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
-        <Marker position={mapPosition}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {cities.map((city) => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id}
+          >
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        ))}
       </MapContainer>
     </div>
   );
