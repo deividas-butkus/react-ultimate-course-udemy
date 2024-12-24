dcf8fa185e2e9e309f5f95c8b9d25e36d6823ec9
deividas-butkus <deividas.butkus@hotmail.com>
2024-12-24 13:08:04 +0200

    11 - WORLDWISE -create custom hook useCities

--- 11-worldwise/contexts/CitiesContexts.jsx
+++ 11-worldwise/contexts/CitiesContexts.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState, useContext, useEffect } from "react";
 
 const BASE_URL = "http://localhost:9000";
 
@@ -36,4 +36,11 @@ function CitiesProvider({ children }) {
   );
 }
 
-export { CitiesProvider };
+function useCities() {
+  const context = useContext(CitiesContext);
+  if (context === undefined)
+    throw new Error("CitiesContext is used outside the CitiesProvider");
+  return context;
+}
+
+export { CitiesProvider, useCities };
