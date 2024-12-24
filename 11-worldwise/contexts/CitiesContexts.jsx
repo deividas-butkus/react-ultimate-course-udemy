251aa249f7c1ce0a60578f70f2c6849c92f99bca
deividas-butkus <deividas.butkus@hotmail.com>
2024-12-24 13:00:00 +0200

    11 - WORLDWISE -create CitieasContext

---
+++ 11-worldwise/contexts/CitiesContexts.jsx
@@ -0,0 +1,39 @@
+import { createContext, useState, useEffect } from "react";
+
+const BASE_URL = "http://localhost:9000";
+
+const CitiesContext = createContext();
+
+function CitiesProvider({ children }) {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const response = await fetch(`${BASE_URL}/cities`);
+        const data = await response.json();
+        setCities(data);
+      } catch {
+        alert("There was an error lpading data...");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
+  return (
+    <CitiesContext.Provider
+      value={{
+        cities,
+        isLoading,
+      }}
+    >
+      {children}
+    </CitiesContext.Provider>
+  );
+}
+
+export { CitiesProvider };
