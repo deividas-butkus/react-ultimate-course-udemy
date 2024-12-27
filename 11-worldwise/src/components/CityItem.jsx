e76091df8386710c174ad0e75a1994f2687179b3
deividas-butkus <deividas.butkus@hotmail.com>
2024-12-27 20:29:43 +0200

    11 - WORLDWISE -apply  deleteCity handling in CityItem

--- 11-worldwise/src/components/CityItem.jsx
+++ 11-worldwise/src/components/CityItem.jsx
@@ -13,6 +13,11 @@ function CityItem({ city }) {
   const { currentCity, deleteCity } = useCities();
   const { cityName, emoji, date, id, position } = city;
 
+  function handleClick(e) {
+    e.preventDefault();
+    deleteCity(id);
+  }
+
   return (
     <li>
       <Link
@@ -24,7 +29,9 @@ function CityItem({ city }) {
         <span className={styles.emoji}>{emoji}</span>
         <h3 className={styles.name}> {cityName}</h3>
         <time className={styles.date}>({formatDate(date)})</time>
-        <button className={styles.deleteBtn}>&times;</button>
+        <button className={styles.deleteBtn} onClick={handleClick}>
+          &times;
+        </button>
       </Link>
     </li>
   );
