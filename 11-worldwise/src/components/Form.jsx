37397996760deb2f7b5057cb6ec3c784565b8f38
deividas-butkus <deividas.butkus@hotmail.com>
2024-12-27 18:37:14 +0200

    11 - WORLDWISE - set geocodingError  to show conditionally instead of Form in case user is clicking on water where is no countryCode

--- 11-worldwise/src/components/Form.jsx
+++ 11-worldwise/src/components/Form.jsx
@@ -7,33 +7,43 @@ import Button from "./Button";
 import BackButton from "./BackButton";
 import { useUrlPosition } from "../hooks/useUrlPosition";
 import { convertToEmoji } from "../utils/emojiUtils";
+import Message from "../components/Message";
+import Spinner from "../components/Spinner";
 
 const BASE_URL = "https://api.bigdatacloud.net/data/reverse-geocode-client";
 
 function Form() {
   const [lat, lng] = useUrlPosition();
-  const [, /* isLoadingGeocoding */ setIsLoadingGeocoding] = useState(false);
+  const [isLoadingGeocoding, setIsLoadingGeocoding] = useState(false);
   const [cityName, setCityName] = useState("");
-  const [, /* country */ setCountry] = useState("");
+  const [, setCountry] = useState("");
   const [date, setDate] = useState(new Date());
   const [notes, setNotes] = useState("");
   const [emoji, setEmoji] = useState("");
+  const [geocodingError, setGeocodingError] = useState("");
 
   useEffect(
     function () {
       async function fetchCityData() {
         try {
           setIsLoadingGeocoding(true);
+          setGeocodingError("");
           const response = await fetch(
             `${BASE_URL}?latitude=${lat}&longitude=${lng}`
           );
           const data = await response.json();
           console.log(data);
+
+          if (!data.countryCode)
+            throw new Error(
+              "That doesn't seem to be a city. Click somewhere else 😊"
+            );
+
           setCityName(data.city || data.locality || "");
           setCountry(data.countryName);
           setEmoji(convertToEmoji(data.countryCode));
         } catch (error) {
-          console.log(error);
+          setGeocodingError(error.message);
         } finally {
           setIsLoadingGeocoding(false);
         }
@@ -42,6 +52,10 @@ function Form() {
     },
     [lat, lng]
   );
+
+  if (isLoadingGeocoding) return <Spinner />;
+
+  if (geocodingError) return <Message message={geocodingError} />;
 
   return (
     <form className={styles.form}>
