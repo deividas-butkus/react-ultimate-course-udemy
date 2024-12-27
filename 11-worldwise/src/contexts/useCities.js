962066ccda3b8e8ca3bb4b6e65769374b67fc2d1
deividas-butkus <deividas.butkus@hotmail.com>
2024-12-27 22:01:49 +0200

    11 - WORLDWISE -correct file name from CitiesContexts to CitiesContext

--- 11-worldwise/src/contexts/useCities.js
+++ 11-worldwise/src/contexts/useCities.js
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import CitiesContext from "./CitiesContexts";
+import { CitiesContext } from "./CitiesContext";
 
 function useCities() {
   const context = useContext(CitiesContext);
