c442c24905959b05c9e731a8f45c5e3c7b8b11bf
deividas-butkus <deividas.butkus@hotmail.com>
2024-12-30 12:08:58 +0200

    11 - WORLDWISE - implement logout functionality in User

--- 11-worldwise/src/components/User.jsx
+++ 11-worldwise/src/components/User.jsx
@@ -1,23 +1,24 @@
+import { useNavigate } from "react-router-dom";
+import useAuth from "../contexts/useAuth";
 import styles from "./User.module.css";
 
-const FAKE_USER = {
-  name: "Jack",
-  email: "jack@example.com",
-  password: "qwerty",
-  avatar: "https://i.pravatar.cc/100?u=zz",
-};
-
 function User() {
-  const user = FAKE_USER;
+  const navigate = useNavigate();
+  const { user, isAuthenticated, logout } = useAuth();
 
-  function handleClick() {}
+  function handleLogout() {
+    logout();
+    navigate("/");
+  }
 
   return (
-    <div className={styles.user}>
-      <img src={user.avatar} alt={user.name} />
-      <span>Welcome, {user.name}</span>
-      <button onClick={handleClick}>Logout</button>
-    </div>
+    isAuthenticated && (
+      <div className={styles.user}>
+        <img src={user.avatar} alt={user.name} />
+        <span>Welcome, {user.name}</span>
+        <button onClick={handleLogout}>Logout</button>
+      </div>
+    )
   );
 }
 
