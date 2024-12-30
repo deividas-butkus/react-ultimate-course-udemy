75543a6522c4cefd7ecbd8172c6820de7819bd1c
deividas-butkus <deividas.butkus@hotmail.com>
2024-12-30 12:31:38 +0200

    11 - WORLDWISE - apply replace on redirecting in Login

--- 11-worldwise/src/pages/Login.jsx
+++ 11-worldwise/src/pages/Login.jsx
@@ -14,7 +14,7 @@ export default function Login() {
 
   useEffect(() => {
     if (isAuthenticated) {
-      navigate("/app");
+      navigate("/app", { replace: true });
     }
   }, [isAuthenticated, navigate]);
 
