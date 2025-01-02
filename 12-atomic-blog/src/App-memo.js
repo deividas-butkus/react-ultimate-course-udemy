fe8b7a1a219583cf5cd6b7f7000dade79fb7a284
deividas-butkus <deividas.butkus@hotmail.com>
2025-01-02 23:04:27 +0200

    12 - ATOMIC_BLOG - make archiveOptions stable and memoize with useMemo

--- 12-atomic-blog/src/App-memo.js
+++ 12-atomic-blog/src/App-memo.js
@@ -1,4 +1,4 @@
-import { useEffect, useState, memo } from "react";
+import { useEffect, useState, memo, useMemo } from "react";
 import { faker } from "@faker-js/faker";
 
 function createRandomPost() {
@@ -41,10 +41,12 @@ function App() {
     [isFakeDark]
   );
 
-  const archiveOptions = {
-    show: false,
-    title: "Post archive in addition to main posts",
-  };
+  const archiveOptions = useMemo(() => {
+    return {
+      show: false,
+      title: "Post archive in addition to main posts",
+    };
+  }, []);
 
   return (
     <section>
