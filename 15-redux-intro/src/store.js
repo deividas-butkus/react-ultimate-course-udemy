aba5084e06aa4221992810ab574ee8d065135eed
deividas-butkus <deividas.butkus@hotmail.com>
2025-01-04 21:37:57 +0200

    15 - REDUX INTRO - create action creator functions

--- 15-redux-intro/src/store.js
+++ 15-redux-intro/src/store.js
@@ -35,15 +35,48 @@ function reducer(state = initialState, action) {
 
 const store = createStore(reducer);
 
-store.dispatch({ type: "account/deposit", payload: 500 });
-store.dispatch({ type: "account/withdraw", payload: 200 });
-console.log(store.getState());
+// store.dispatch({ type: "account/deposit", payload: 500 });
+// store.dispatch({ type: "account/withdraw", payload: 200 });
+// console.log(store.getState());
 
-store.dispatch({
-  type: "account/requestLoan",
-  payload: { amount: 1000, purpose: "Buy a car" },
-});
-console.log(store.getState());
+// store.dispatch({
+//   type: "account/requestLoan",
+//   payload: { amount: 1000, purpose: "Buy a car" },
+// });
+// console.log(store.getState());
 
-store.dispatch({ type: "account/payLoan" });
+// store.dispatch({ type: "account/payLoan" });
+// console.log(store.getState());
+
+// const ACCOUNT_DEPOSIT = "account/deposit"
+// * older React Redux syntax to have a central place for type, but modern apps do not use it
+
+function deposit(amount) {
+  return { type: "account/deposit", payload: amount };
+}
+
+function withdraw(amount) {
+  return { type: "account/withdraw", payload: amount };
+}
+
+function requestLoan(amount, purpose) {
+  return {
+    type: "account/requestLoan",
+    payload: { amount, purpose },
+  };
+}
+
+function payLoan() {
+  return {
+    type: "account/payLoan",
+  };
+}
+
+store.dispatch(deposit(500));
+console.log(store.getState());
+store.dispatch(withdraw(200));
+console.log(store.getState());
+store.dispatch(requestLoan(1000, "Buy a car"));
+console.log(store.getState());
+store.dispatch(payLoan());
 console.log(store.getState());
