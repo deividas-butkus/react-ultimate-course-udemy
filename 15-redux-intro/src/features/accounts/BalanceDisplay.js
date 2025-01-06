d807ddb440411256f2586a4c2402e6dee22dcd0e
deividas-butkus <deividas.butkus@hotmail.com>
2025-01-06 17:13:44 +0200

    15 - REDUX INTRO -use pre-hook way of connecting Redux to component in BalanceDisplay

--- 15-redux-intro/src/features/accounts/BalanceDisplay.js
+++ 15-redux-intro/src/features/accounts/BalanceDisplay.js
@@ -7,8 +7,14 @@ function formatCurrency(value) {
   }).format(value);
 }
 
-function BalanceDisplay() {
-  return <div className="balance">{formatCurrency(123456)}</div>;
+function BalanceDisplay({ balance }) {
+  return <div className="balance">{formatCurrency(balance)}</div>;
 }
 
-export default BalanceDisplay;
+function mapStateToProps(state) {
+  return {
+    balance: state.account.balance,
+  };
+}
+
+export default connect(mapStateToProps)(BalanceDisplay);
