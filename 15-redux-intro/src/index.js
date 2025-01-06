67d3d14f281f61c4abcb8d0fdd9622dde1b42d48
deividas-butkus <deividas.butkus@hotmail.com>
2025-01-06 15:20:50 +0200

    15 - REDUX INTRO -use Provider and connect redux store to it

--- 15-redux-intro/src/index.js
+++ 15-redux-intro/src/index.js
@@ -12,6 +12,8 @@ console.log(store.getState());
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
