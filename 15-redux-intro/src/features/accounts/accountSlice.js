f882b3c06f3a0ef8656df58834005b908df8ea51
deividas-butkus <deividas.butkus@hotmail.com>
2025-01-07 10:02:26 +0200

    15 - REDUX INTRO -replace accountReducer function by accountSlice variable based on createSlice() in accountSlice

--- 15-redux-intro/src/features/accounts/accountSlice.js
+++ 15-redux-intro/src/features/accounts/accountSlice.js
@@ -7,6 +7,47 @@ const initialState = {
   isLoading: false,
 };
 
+const accountSlice = createSlice({
+  name: "account",
+  initialState,
+  reducers: {
+    deposit(state, action) {
+      state.balance += action.payload;
+    },
+    withdraw(state, action) {
+      state.balance -= action.payload;
+    },
+    requestLoan: {
+      prepare(amount, purpose) {
+        return {
+          payload: { amount, purpose },
+        };
+      },
+      reducer(state, action) {
+        if (state.loan > 0) return;
+
+        state.loan = action.payload.amount;
+        state.loanPurpose = action.payload.purpose;
+        state.balance = state.balance + action.payload.amount;
+      },
+    },
+    payLoan(state, action) {
+      state.loanPurpose = "";
+      state.balance -= state.loan;
+      state.loan = 0;
+    },
+  },
+});
+
+console.log(accountSlice);
+
+export const { deposit, withdraw, requestLoan, payLoan } = accountSlice.actions;
+
+console.log(requestLoan(1000, "Buy car"));
+
+export default accountSlice.reducer;
+
+/*
 export default function accountReducer(state = initialStateAccount, action) {
   switch (action.type) {
     case "account/deposit":
@@ -73,3 +114,4 @@ export function payLoan() {
     type: "account/payLoan",
   };
 }
+  */
