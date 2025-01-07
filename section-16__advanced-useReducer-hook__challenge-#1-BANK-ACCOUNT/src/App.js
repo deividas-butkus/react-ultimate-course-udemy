33ec9d1bbfdc0e3c8af9e9578204cade4e2956d9
deividas-butkus <deividas.butkus@hotmail.com>
2025-01-07 16:46:09 +0200

    section-16__BANK-ACCOUNT - set !isActive on Deposit 150 button in App

--- section-16__advanced-useReducer-hook__challenge-#1-BANK-ACCOUNT/src/App.js
+++ section-16__advanced-useReducer-hook__challenge-#1-BANK-ACCOUNT/src/App.js
@@ -64,7 +64,7 @@ export default function App() {
         </button>
       </p>
       <p>
-        <button onClick={() => {}} disabled={false}>
+        <button onClick={() => {}} disabled={!isActive}>
           Deposit 150
         </button>
       </p>
