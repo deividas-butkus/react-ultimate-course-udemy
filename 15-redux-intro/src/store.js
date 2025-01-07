038554d2ed575bad6b050bd2a94f69478b95cc78
deividas-butkus <deividas.butkus@hotmail.com>
2025-01-07 10:24:36 +0200

    15 - REDUX INTRO -replace rootReducer with store based on configureStore() in store.js

--- 15-redux-intro/src/store.js
+++ 15-redux-intro/src/store.js
@@ -3,9 +3,11 @@ import { configureStore } from "@reduxjs/toolkit";
 import accountReducer from "./features/accounts/accountSlice";
 import customerReducer from "./features/customers/customerSlice";
 
-const rootReducer = combineReducers({
-  account: accountReducer,
-  customer: customerReducer,
+const store = configureStore({
+  reducer: {
+    account: accountReducer,
+    customer: customerReducer,
+  },
 });
 
 const store = createStore(
