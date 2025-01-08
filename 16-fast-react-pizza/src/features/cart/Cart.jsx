6dcff86262dd2f7aa5948c0b7c1746685119c260
deividas-butkus <deividas.butkus@hotmail.com>
2025-01-08 19:25:48 +0200

    16 - FAST REACT PIZZA -use cart temporarilly to get rid of eslint errors

--- 16-fast-react-pizza/src/features/cart/Cart.jsx
+++ 16-fast-react-pizza/src/features/cart/Cart.jsx
@@ -1,23 +1,23 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const fakeCart = [
   {
     pizzaId: 12,
-    name: 'Mediterranean',
+    name: "Mediterranean",
     quantity: 2,
     unitPrice: 16,
     totalPrice: 32,
   },
   {
     pizzaId: 6,
-    name: 'Vegetale',
+    name: "Vegetale",
     quantity: 1,
     unitPrice: 13,
     totalPrice: 13,
   },
   {
     pizzaId: 11,
-    name: 'Spinach and Mushroom',
+    name: "Spinach and Mushroom",
     quantity: 1,
     unitPrice: 15,
     totalPrice: 15,
@@ -32,6 +32,7 @@ function Cart() {
       <Link to="/menu">&larr; Back to menu</Link>
 
       <h2>Your cart, %NAME%</h2>
+      <h3>{cart}</h3>
 
       <div>
         <Link to="/order/new">Order pizzas</Link>
