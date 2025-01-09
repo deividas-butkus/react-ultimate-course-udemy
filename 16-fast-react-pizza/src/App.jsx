b01d83b3d756b6885145465df6289a4900e2a971
deividas-butkus <deividas.butkus@hotmail.com>
2025-01-09 22:37:48 +0200

    16 - FAST REACT PIZZA - use menuLoader, and React v7 future flags in App

--- 16-fast-react-pizza/src/App.jsx
+++ 16-fast-react-pizza/src/App.jsx
@@ -7,33 +7,42 @@ import CreateOrder from "./features/order/CreateOrder";
 import Order from "./features/order/Order";
 import AppLayout from "./ui/AppLayout";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      element: <AppLayout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/menu",
+          element: <Menu />,
+          loader: menuLoader,
+        },
+        {
+          path: "/cart",
+          element: <Cart />,
+        },
+        {
+          path: "/order/new",
+          element: <CreateOrder />,
+        },
+        {
+          path: "/order/:orderId",
+          element: <Order />,
+        },
+      ],
+    },
+  ],
   {
-    element: <AppLayout />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/menu",
-        element: <Menu />,
-      },
-      {
-        path: "/cart",
-        element: <Cart />,
-      },
-      {
-        path: "/order/new",
-        element: <CreateOrder />,
-      },
-      {
-        path: "/oder/:orderId",
-        element: <Order />,
-      },
-    ],
-  },
-]);
+    future: {
+      v7_startTransition: true, // Opt into startTransition behavior
+      v7_relativeSplatPath: true, // Opt into relative splat path behavior
+    },
+  }
+);
 
 function App() {
   return <RouterProvider router={router} />;
