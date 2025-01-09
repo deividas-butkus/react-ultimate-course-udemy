bac83c206c8a8bf163ac19ec43fea0d1e4e91b52
deividas-butkus <deividas.butkus@hotmail.com>
2025-01-09 22:50:17 +0200

    16 - FAST REACT PIZZA - import Loader to AppLayout

--- 16-fast-react-pizza/src/ui/AppLayout.jsx
+++ 16-fast-react-pizza/src/ui/AppLayout.jsx
@@ -2,6 +2,7 @@ import { Outlet, useNavigation } from "react-router-dom";
 
 import Header from "./Header";
 import CartOverview from "../features/cart/CartOverview";
+import Loader from "./Loader";
 
 function AppLayout() {
   return (
