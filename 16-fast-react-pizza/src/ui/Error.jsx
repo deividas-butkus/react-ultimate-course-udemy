4960613f5139ce0579d75f5e6c7a8c8fd5ca58f0
Deividas Butkus <d.butkus@godeltech.com>
2025-05-21 14:42:57 +0300

    16 - FAST REACT PIZZA - refactor LinkButton tp avoid magic value "-1" and be more flexible, semantically clear, and easier to maintain

--- 16-fast-react-pizza/src/ui/Error.jsx
+++ 16-fast-react-pizza/src/ui/Error.jsx
@@ -1,14 +1,16 @@
-import { useRouteError } from "react-router-dom";
+import { useRouteError, useNavigate } from "react-router-dom";
 import LinkButton from "./LinkButton";
 
 function NotFound() {
   const error = useRouteError();
 
+  const navigate = useNavigate();
+
   return (
     <div>
       <h1>Something went wrong 😢</h1>
       <p>{error.data || error.message}</p>
-      <LinkButton to="-1">&larr; Go back</LinkButton>
+      <LinkButton onClick={() => navigate(-1)}>&larr; Go back</LinkButton>
     </div>
   );
 }
--- 16-fast-react-pizza/src/ui/LinkButton.jsx
+++ 16-fast-react-pizza/src/ui/LinkButton.jsx
@@ -1,12 +1,11 @@
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-const LinkButton = ({ children, to }) => {
-  const navigate = useNavigate();
+const LinkButton = ({ children, to, onClick }) => {
   const className = "text-sm text-blue-500 hover:text-blue-600 hover:underline";
 
-  if (to === "-1")
+  if (onClick)
     return (
-      <button className={className} onClick={() => navigate(-1)}>
+      <button className={className} onClick={onClick}>
         {children}
       </button>
     );
