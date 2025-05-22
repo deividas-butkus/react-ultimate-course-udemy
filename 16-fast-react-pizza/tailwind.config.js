7345be736ed01056423ba0eed21ccbe4504c822f
Deividas Butkus <d.butkus@godeltech.com>
2025-05-22 13:29:14 +0300

    16 - FAST REACT PIZZA - add theme extending props to tailwind config

--- 16-fast-react-pizza/tailwind.config.js
+++ 16-fast-react-pizza/tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        pizza: "#123456",
+      },
+      fontSize: {
+        huge: ["10rem", { lineHeight: "1" }],
+      },
+      height: {
+        screen: "100dvh",
+      },
+    },
     fontFamily: {
       sans: "Roboto Mono, monospace",
     },
