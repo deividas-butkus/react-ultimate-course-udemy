255b33f1646a831824d24e12891a38fdad6e0a11
Deividas Butkus <d.butkus@godeltech.com>
2025-05-22 11:42:35 +0300

    16 - FAST REACT PIZZA -create 'pizza' fontFamily in tailwind config

--- 16-fast-react-pizza/tailwind.config.js
+++ 16-fast-react-pizza/tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      pizza: "Roboto Mono, monospace",
+    },
   },
   plugins: [],
 };
