48f0ce5534272a39a0949870a0f1286b054b95e6
Deividas Butkus <d.butkus@godeltech.com>
2025-05-26 16:22:19 +0300

    Apply state for showing FormAddFriend on Button click

--- 06-1-eat-n-split/src/App.js
+++ 06-1-eat-n-split/src/App.js
@@ -1,10 +1,24 @@
+import { useState } from "react";
+
+import Button from "./ui/Button";
+import FormAddFriend from "./ui/FormAddFriend";
 import FriendList from "./ui/FriendList";
 
 const App = () => {
+  const [formAddFriendVisible, setFormAddFriendVisible] = useState(false);
+
+  const handleAddFriendClick = () => {
+    setFormAddFriendVisible(!formAddFriendVisible);
+  };
+
   return (
     <div className="app">
       <div className="sidebar">
         <FriendList />
+        {formAddFriendVisible && <FormAddFriend />}
+        <Button onClick={handleAddFriendClick}>
+          {formAddFriendVisible ? "Close" : "Add new friend"}
+        </Button>
       </div>
     </div>
   );
