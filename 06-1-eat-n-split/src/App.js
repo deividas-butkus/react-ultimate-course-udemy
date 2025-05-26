badabb11a87b8be0ae5058a7804ae1d512c91418
Deividas Butkus <d.butkus@godeltech.com>
2025-05-26 15:21:33 +0300

    Use FriendList in App

---
+++ 06-1-eat-n-split/src/App.js
@@ -0,0 +1,13 @@
+import FriendList from "./ui/FriendList";
+
+const App = () => {
+  return (
+    <div className="app">
+      <div className="sidebar">
+        <FriendList />
+      </div>
+    </div>
+  );
+};
+
+export default App;
