4652006a5638b30a62ac832e1d29d264f2e0bd7b
Deividas Butkus <d.butkus@godeltech.com>
2025-05-27 12:55:07 +0300

    Pass through onSelectFriend and selectedFriend props in FriendList

--- 06-1-eat-n-split/src/ui/FriendList.jsx
+++ 06-1-eat-n-split/src/ui/FriendList.jsx
@@ -1,11 +1,16 @@
 import Friend from "../ui/Friend";
 
-const FriendList = ({ friends }) => {
+const FriendList = ({ friends, onSelectFriend, selectedFriend }) => {
   return (
     <div className="sidebar">
       <ul>
         {friends.map((friend) => (
-          <Friend friend={friend} key={friend.id} />
+          <Friend
+            friend={friend}
+            key={friend.id}
+            selectedFriend={selectedFriend}
+            onSelect={onSelectFriend}
+          />
         ))}
       </ul>
     </div>
