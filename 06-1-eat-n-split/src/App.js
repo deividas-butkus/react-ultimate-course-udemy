50034d01bba124d35f4a60cc6496de98c4d432dd
Deividas Butkus <d.butkus@godeltech.com>
2025-05-27 11:10:19 +0300

    Change state name for better readability

--- 06-1-eat-n-split/src/App.js
+++ 06-1-eat-n-split/src/App.js
@@ -11,7 +11,7 @@ const App = () => {
   const [formAddFriendVisible, setFormAddFriendVisible] = useState(false);
 
   const handleAddFriendClick = () => {
-    setFormAddFriendVisible((visible) => !visible);
+    setFormAddFriendVisible((visibility) => !visibility);
   };
 
   const handleAddFriend = (newFriend) => {
