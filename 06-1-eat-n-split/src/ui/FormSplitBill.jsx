1c2bf2c43afa5043686cf9d099adf7d4919896fc
Deividas Butkus <d.butkus@godeltech.com>
2025-05-27 13:20:02 +0300

    Add emojis to FromSplitBill labels

--- 06-1-eat-n-split/src/ui/FormSplitBill.jsx
+++ 06-1-eat-n-split/src/ui/FormSplitBill.jsx
@@ -5,22 +5,22 @@ const FormSplitBill = ({ selectedFriend }) => {
     <form className="form-split-bill">
       <h2>Split a bill with {selectedFriend.name}</h2>
 
-      <label htmlFor="billValue">Bill value</label>
+      <label htmlFor="billValue">💰Bill value</label>
       <input name="billValue" id="billValue" type="text" />
 
-      <label htmlFor="yourExpense">Your expense</label>
+      <label htmlFor="yourExpense">🫵Your expense</label>
       <input name="yourExpense" id="yourExpense" type="text" />
 
-      <label htmlFor="friendExpense">X's expense</label>
+      <label htmlFor="friendExpense">🤝{selectedFriend.name}'s expense</label>
       <input name="friendExpense" id="friendExpense" type="text" disabled />
 
-      <label htmlFor="who">Who is paying the bill</label>
+      <label htmlFor="who">❓Who is paying the bill</label>
       <select name="who" id="who" defaultValue="">
         <option value="" disabled>
           Select
         </option>
         <option value="user">You</option>
-        <option value="friend">X</option>
+        <option value="friend">{selectedFriend.name}</option>
       </select>
 
       <Button type="submit">Split bill</Button>
