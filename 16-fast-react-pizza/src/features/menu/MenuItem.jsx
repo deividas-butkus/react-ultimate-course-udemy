3143bb1d32bdd1ee7e0d3d75facc5b1a33885b1b
Deividas Butkus <d.butkus@godeltech.com>
2025-05-28 18:35:04 +0300

    Use DeleteItem component and getCurrentQuantityById method to conditionally render adding to cart and deleting from cart buttons

--- 16-fast-react-pizza/src/features/menu/MenuItem.jsx
+++ 16-fast-react-pizza/src/features/menu/MenuItem.jsx
@@ -1,12 +1,18 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 import { formatCurrency } from "../../utils/helpers";
 import Button from "../../ui/Button";
 import { addItem } from "../cart/cartSlice";
+import DeleteItem from "../cart/DeleteItem";
+import { getCurrentQuantityById } from "../cart/cartSlice";
 
 function MenuItem({ pizza }) {
-  const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
   const dispatch = useDispatch();
+
+  const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+
+  const currentQuantity = useSelector(getCurrentQuantityById(id));
+  const isInCart = currentQuantity > 0;
 
   const handleAddToCart = () => {
     const newItem = {
@@ -40,7 +46,10 @@ function MenuItem({ pizza }) {
               Sold out
             </p>
           )}
-          {!soldOut && (
+
+          {isInCart && <DeleteItem pizzaId={id} />}
+
+          {!soldOut && !isInCart && (
             <Button type="small" onClick={handleAddToCart}>
               Add to cart
             </Button>
