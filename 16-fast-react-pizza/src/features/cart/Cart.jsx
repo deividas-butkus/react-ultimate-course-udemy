2622a8a1a9d70b48910638b0b28e03252decf333
Deividas Butkus <d.butkus@godeltech.com>
2025-05-28 17:29:37 +0300

    Use getCart and getUsername funtions from store in Cart,

--- 16-fast-react-pizza/src/features/cart/Cart.jsx
+++ 16-fast-react-pizza/src/features/cart/Cart.jsx
@@ -2,34 +2,12 @@ import { useSelector } from "react-redux";
 import CartItem from "./CartItem";
 import LinkButton from "../../ui/LinkButton";
 import Button from "../../ui/Button";
-
-const fakeCart = [
-  {
-    pizzaId: 12,
-    name: "Mediterranean",
-    quantity: 2,
-    unitPrice: 16,
-    totalPrice: 32,
-  },
-  {
-    pizzaId: 6,
-    name: "Vegetale",
-    quantity: 1,
-    unitPrice: 13,
-    totalPrice: 13,
-  },
-  {
-    pizzaId: 11,
-    name: "Spinach and Mushroom",
-    quantity: 1,
-    unitPrice: 15,
-    totalPrice: 15,
-  },
-];
+import { getCart } from "./cartSlice";
+import { getUsername } from "../user/userSlice";
 
 function Cart() {
-  const cart = fakeCart;
-  const username = useSelector((state) => state.user.username);
+  const username = useSelector(getUsername);
+  const cart = useSelector(getCart);
 
   return (
     <div className="px-4 py-3">
