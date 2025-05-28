684ee9e2fc5f06fb24d4cafbba8a8e408ee874e2
Deividas Butkus <d.butkus@godeltech.com>
2025-05-28 16:35:44 +0300

    Apply addItem action on MenuItem

--- 16-fast-react-pizza/src/features/menu/MenuItem.jsx
+++ 16-fast-react-pizza/src/features/menu/MenuItem.jsx
@@ -1,8 +1,24 @@
+import { useDispatch } from "react-redux";
+
 import { formatCurrency } from "../../utils/helpers";
 import Button from "../../ui/Button";
+import { addItem } from "../cart/cartSlice";
 
 function MenuItem({ pizza }) {
-  const { name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+  const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+  const dispatch = useDispatch();
+
+  const handleAddToCart = () => {
+    const newItem = {
+      pizzaId: id,
+      name,
+      quantity: 1,
+      unitPrice,
+      totalPrice: unitPrice * 1,
+    };
+
+    dispatch(addItem(newItem));
+  };
 
   return (
     <li className="flex gap-4">
@@ -24,7 +40,11 @@ function MenuItem({ pizza }) {
               Sold out
             </p>
           )}
-          {!soldOut && <Button type="small">Add to cart</Button>}
+          {!soldOut && (
+            <Button type="small" onClick={handleAddToCart}>
+              Add to cart
+            </Button>
+          )}
         </div>
       </div>
     </li>
