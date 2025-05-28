4bd124c4764aace531f5bfdef756eeaae571be6e
Deividas Butkus <d.butkus@godeltech.com>
2025-05-28 17:18:20 +0300

    Add getTotalCartQuantity and getTotalCartPrice to cartSlice

--- 16-fast-react-pizza/src/features/cart/cartSlice.js
+++ 16-fast-react-pizza/src/features/cart/cartSlice.js
@@ -1,17 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  // cart: [],
-
-  cart: [
-    {
-      pizzaId: 123456,
-      name: "Mediterranean",
-      quantity: 2,
-      unitPrice: 16,
-      totalPrice: 32,
-    },
-  ],
+  cart: [],
 };
 
 const cartSlice = createSlice({
@@ -60,3 +50,9 @@ export const {
 } = cartSlice.actions;
 
 export default cartSlice.reducer;
+
+export const getTotalCartQuantity = (state) =>
+  state.cart.cart.reduce((acc, cur) => acc + cur.quantity, 0);
+
+export const getTotalCartPrice = (state) =>
+  state.cart.cart.reduce((acc, cur) => acc + cur.totalPrice, 0);
