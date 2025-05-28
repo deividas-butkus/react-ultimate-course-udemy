015986ebcbfd9b6e2be4cd31915b29f4333ba62e
Deividas Butkus <d.butkus@godeltech.com>
2025-05-28 18:32:03 +0300

    Correct typo in key

--- 16-fast-react-pizza/src/features/cart/Cart.jsx
+++ 16-fast-react-pizza/src/features/cart/Cart.jsx
@@ -25,7 +25,7 @@ function Cart() {
 
       <ul className="mt-3 divide-y divide-stone-200 border-b">
         {cart.map((item) => (
-          <CartItem key={item.key} item={item} />
+          <CartItem key={item.id} item={item} />
         ))}
       </ul>
 
