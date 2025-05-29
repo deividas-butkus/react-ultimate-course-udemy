674586cf2d999489ccfaba4d6d1b8142be32c5ff
deividas-butkus <deividas.butkus@hotmail.com>
2025-05-29 12:56:47 +0300

    Make key unique in Order

--- 16-fast-react-pizza/src/features/order/Order.jsx
+++ 16-fast-react-pizza/src/features/order/Order.jsx
@@ -53,8 +53,11 @@ function Order() {
       </div>
 
       <ul className="divide-y divide-stone-200 border-b border-t">
-        {cart.map((item) => (
-          <OrderItem item={item} key={item.id} />
+        {cart.map((item, index) => (
+          <OrderItem
+            item={item}
+            key={item.id ? `${item.id}-${index}` : index}
+          />
         ))}
       </ul>
 
