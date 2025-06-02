748de8a661400040d0cdb2d1b919f8e770b7798f
Deividas Butkus <d.butkus@godeltech.com>
2025-06-02 12:15:53 +0300

    Add Logo to RootLayout

--- 21-the-wild-oasis-website/app/layout.js
+++ 21-the-wild-oasis-website/app/layout.js
@@ -1,11 +1,20 @@
+import Logo from "./components/Logo";
 import Navigation from "./components/Navigation";
+
+export const metadata = {
+  title: " The wild Oasis",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <Navigation />
+        <header>
+          <Logo />
+          <Navigation />
+        </header>
         <main>{children}</main>
+        <footet>Copyright by The Wild Oasis</footet>
       </body>
     </html>
   );
