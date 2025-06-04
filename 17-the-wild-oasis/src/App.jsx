a980549a76b5f120cba914dc412484cf0d639467
Deividas Butkus <d.butkus@godeltech.com>
2025-06-04 20:35:10 +0300

    Move Button and Input from App to UI components

--- 17-the-wild-oasis/src/App.jsx
+++ 17-the-wild-oasis/src/App.jsx
@@ -9,24 +9,6 @@ const H1 = styled.h1`
   background-color: yellow;
 `;
 
-const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem 1.6rem;
-  font-weight: 500;
-  border: none;
-  border-radius: 7px;
-  background-color: purple;
-  color: white;
-  margin: 1rem;
-  cursor: pointer;
-`;
-
-const Input = styled.input`
-  border 1px solid #ddd;
-  border-radius: 5px;
-  padding: 0.8rem 1.2rem;
-`;
-
 const StyledApp = styled.main`
   background-color: orangered;
   padding: 20px;
