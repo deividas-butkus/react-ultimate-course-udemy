c8ad6d0bb3c399d60578f485f4692fc63b82fb8b
deividas-butkus <deividas.butkus@hotmail.com>
2025-06-29 16:31:02 +0300

    Add booking route

--- 17-the-wild-oasis/src/App.jsx
+++ 17-the-wild-oasis/src/App.jsx
@@ -6,6 +6,7 @@ import { Toaster } from "react-hot-toast";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Bookings from "./pages/Bookings";
+import Booking from "./pages/Booking";
 import Cabins from "./pages/Cabins";
 import Users from "./pages/Users";
 import Settings from "./pages/Settings";
@@ -38,6 +39,7 @@ function App() {
             <Route index element={<Navigate replace to="dashboard" />} />
             <Route path="dashboard" element={<Dashboard />} />
             <Route path="bookings" element={<Bookings />} />
+            <Route path="bookings/:bookingId" element={<Booking />} />
             <Route path="users" element={<Users />} />
             <Route path="cabins" element={<Cabins />} />
             <Route path="settings" element={<Settings />} />
