6bec39c52423dd15a1ceeaabcd2a2393107a9dbb
deividas-butkus <deividas.butkus@hotmail.com>
2025-07-14 11:34:50 +0300

    Use SalesChart in DashboardLayout

--- 17-the-wild-oasis/src/features/dashboard/DashboardLayout.jsx
+++ 17-the-wild-oasis/src/features/dashboard/DashboardLayout.jsx
@@ -4,6 +4,7 @@ import { useRecentStays } from "./useRecentStays";
 import Spinner from "../../ui/Spinner";
 import Stats from "./Stats";
 import { useCabins } from "../cabins/useCabins";
+import SalesChart from "./SalesChart";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -29,7 +30,7 @@ function DashboardLayout() {
       />
       <div>Today&apos;s activity</div>
       <div>Chart for stay durations</div>
-      <div>Chart of sales</div>
+      <SalesChart />
     </StyledDashboardLayout>
   );
 }
