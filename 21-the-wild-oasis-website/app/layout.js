359278e8ce555e3de8abb8fe9b6842877ca54d8a
deividas-butkus <deividas.butkus@hotmail.com>
2025-07-20 16:40:48 +0300

    Apply initial styles on body

--- 21-the-wild-oasis-website/app/layout.js
+++ 21-the-wild-oasis-website/app/layout.js
@@ -9,7 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className="bg-primary-950 text-primary-200 min-h-screen">
         <header>
           <Logo />
           <Navigation />
