3a8515ad23b603022ec8432db7f8e986bd367fc1
deividas-butkus <deividas.butkus@hotmail.com>
2025-07-20 16:00:12 +0300

    Import colors and correct components folder name to _components

--- 21-the-wild-oasis-website/tailwind.config.mjs
+++ 21-the-wild-oasis-website/tailwind.config.mjs
@@ -1,8 +1,11 @@
+import colors from "./app/_styles/colors.json" assert { type: "json" };
+
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./_components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
@@ -10,6 +13,7 @@ module.exports = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        ...colors,
       },
     },
   },
