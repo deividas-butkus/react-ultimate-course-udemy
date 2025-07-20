e7b1c03198ba942f1141c6fc0de5e53604c35f13
deividas-butkus <deividas.butkus@hotmail.com>
2025-07-20 16:21:50 +0300

    Clean up cabins page from test data fetching and displaying

--- 21-the-wild-oasis-website/app/cabins/page.js
+++ 21-the-wild-oasis-website/app/cabins/page.js
@@ -1,31 +1,9 @@
 import Counter from "@/_components/Counter";
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-
 export default async function Page() {
-  let data = [];
-
-  try {
-    const res = await fetch("https://jsonplaceholder.typicode.com/users");
-
-    if (!res.ok) {
-      throw new Error(`Fetch failed with status ${res.status}`);
-    }
-
-    data = await res.json();
-  } catch (error) {
-    console.error("Error fetching users:", error);
-  }
-
   return (
     <div>
       <h1>Cabins page</h1>
-      <ul>
-        {data.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
-      <Counter users={data} />
     </div>
   );
 }
