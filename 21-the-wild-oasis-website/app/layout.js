1da9aae4f8ef80ba767fc177947db4ec751afb90
deividas-butkus <deividas.butkus@hotmail.com>
2025-07-21 19:52:44 +0300

    Add relative position to bg Image parent - body

--- 21-the-wild-oasis-website/app/layout.js
+++ 21-the-wild-oasis-website/app/layout.js
@@ -26,7 +26,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${josefin.className} antialiased bg-primary-950 text-primary-200 min-h-screen flex flex-col`}
+        className={`${josefin.className} antialiased bg-primary-950 text-primary-200 min-h-screen flex flex-col relative`}
       >
         <Header />
         <div className="flex-1 blue-500 px-8 py-12">
