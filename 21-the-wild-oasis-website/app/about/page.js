00abddf9bcf9fa57c6ac799359ebcce04e450094
deividas-butkus <deividas.butkus@hotmail.com>
2025-07-21 13:43:02 +0300

    Remove description test from About page

--- 21-the-wild-oasis-website/app/about/page.js
+++ 21-the-wild-oasis-website/app/about/page.js
@@ -1,7 +1,5 @@
 export const metadata = {
   title: "About",
-  description:
-    "about about about about about about about about about about about about about about about about about about about about about",
 };
 
 export default function Page() {
