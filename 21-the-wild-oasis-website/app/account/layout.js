f00528ff15930b7945da6b76677e52ff1e5a569d
deividas-butkus <deividas.butkus@hotmail.com>
2025-07-22 13:42:58 +0300

    Use SideNavigation in Layout

--- 21-the-wild-oasis-website/app/account/layout.js
+++ 21-the-wild-oasis-website/app/account/layout.js
@@ -1,7 +1,9 @@
+import SideNavigation from "@/_components/SideNavigation";
+
 export default function Layout({ children }) {
   return (
     <div className="grid grid-cols-[16rem_1fr] h-full gap-12">
-      <div>Navigation</div>
+      <SideNavigation />
       <div className="py-1">{children}</div>
     </div>
   );
