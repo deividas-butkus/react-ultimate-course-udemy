6442b9e52b328c91e22a6c1291d6f0fc5475304c
deividas-butkus <deividas.butkus@hotmail.com>
2025-07-24 13:57:09 +0300

    Remove static metadata for website title

--- 21-the-wild-oasis-website/app/cabins/[cabinId]/page.js
+++ 21-the-wild-oasis-website/app/cabins/[cabinId]/page.js
@@ -2,10 +2,6 @@ import { getCabin } from "@/_lib/data-service";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 
-// export const metadata = {
-//   title: `Cabin`,
-// };
-
 export async function generateMetadata({ params }) {
   const { name } = await getCabin(params.cabinId);
 
