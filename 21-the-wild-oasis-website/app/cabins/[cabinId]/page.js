443c97d4f439217c7c3ca7ba54fea4a31f32f2a6
deividas-butkus <deividas.butkus@hotmail.com>
2025-07-24 18:45:10 +0300

    Comment out generateStsticParams to test PPR

--- 21-the-wild-oasis-website/app/cabins/[cabinId]/page.js
+++ 21-the-wild-oasis-website/app/cabins/[cabinId]/page.js
@@ -8,12 +8,12 @@ export async function generateMetadata({ params }) {
   return { title: `Cabin ${name}` };
 }
 
-export async function generateStaticParams() {
-  const cabins = await getCabins();
-  const ids = cabins.map((cabin) => ({ cabinId: String(cabin.id) }));
+// export async function generateStaticParams() {
+//   const cabins = await getCabins();
+//   const ids = cabins.map((cabin) => ({ cabinId: String(cabin.id) }));
 
-  return ids;
-}
+//   return ids;
+// }
 
 export default async function Page({ params }) {
   const cabin = await getCabin(params.cabinId);
