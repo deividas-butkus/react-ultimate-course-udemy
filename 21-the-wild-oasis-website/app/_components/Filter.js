ab80eed6890266297d737a0cf4406d38bb890c1e
deividas-butkus <deividas.butkus@hotmail.com>
2025-07-25 22:42:55 +0300

    Implement handleFilter functionality

--- 21-the-wild-oasis-website/app/_components/Filter.js
+++ 21-the-wild-oasis-website/app/_components/Filter.js
@@ -1,12 +1,32 @@
 "use client";
 
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+
 function Filter() {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const pathname = usePathname();
+
+  function handleFilter(filter) {
+    const params = new URLSearchParams(searchParams);
+    params.set("capacity", filter);
+    router.replace(`${pathname}?${params.toString()}`, { scrall: false });
+  }
+
   return (
     <div className="border border-primary-800 flex">
-      <button className="px-5 py-2">All cabins</button>
-      <button className="px-5 py-2">1&mdash;3 guests</button>
-      <button className="px-5 py-2">4&mdash;7 guests</button>
-      <button className="px-5 py-2">8&mdash;12 guests</button>
+      <button className="px-5 py-2" onClick={() => handleFilter("all")}>
+        All cabins
+      </button>
+      <button className="px-5 py-2" onClick={() => handleFilter("small")}>
+        1&mdash;3 guests
+      </button>
+      <button className="px-5 py-2" onClick={() => handleFilter("medium")}>
+        4&mdash;7 guests
+      </button>
+      <button className="px-5 py-2" onClick={() => handleFilter("large")}>
+        8&mdash;12 guests
+      </button>
     </div>
   );
 }
