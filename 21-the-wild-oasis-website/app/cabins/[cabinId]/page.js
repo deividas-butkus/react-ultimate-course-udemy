b74b8d19b2eab9d09c0dc2f197e60d46a0e28991
deividas-butkus <deividas.butkus@hotmail.com>
2025-07-26 13:20:36 +0300

    Remove getSettings and getBookedDatesByCabinId as moved to Reservation component, and pass cabin prop to Reservation component

--- 21-the-wild-oasis-website/app/cabins/[cabinId]/page.js
+++ 21-the-wild-oasis-website/app/cabins/[cabinId]/page.js
@@ -1,11 +1,6 @@
 import Reservation from "@/_components/Reservation";
 import TextExpander from "@/_components/TextExpander";
-import {
-  getBookedDatesByCabinId,
-  getCabin,
-  getCabins,
-  getSettings,
-} from "@/_lib/data-service";
+import { getCabin, getCabins } from "@/_lib/data-service";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 
@@ -23,15 +18,7 @@ export async function generateStaticParams() {
 }
 
 export default async function Page({ params }) {
-  // const cabin = await getCabin(params.cabinId);
-  // const settings = await getSettings();
-  // const bookedDays = await getBookedDatesByCabinId(params.cabinId);
-
-  const [cabin, settings, bookedDays] = await Promise.all([
-    getCabin(params.cabinId),
-    getSettings(),
-    getBookedDatesByCabinId(params.cabinId),
-  ]);
+  const cabin = await getCabin(params.cabinId);
 
   const { id, name, maxCapacity, regularPrice, discount, image, description } =
     cabin;
@@ -86,7 +73,7 @@ export default async function Page({ params }) {
         <h2 className="text-5xl font-semibold text-center mb-10 text-accent-400">
           Reserve {name} today. Pay on arrival.
         </h2>
-        <Reservation />
+        <Reservation cabin={cabin} />
       </div>
     </div>
   );
