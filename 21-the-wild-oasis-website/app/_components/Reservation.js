52fd0c508df973b78c92e697727638bcefc6914a
deividas-butkus <deividas.butkus@hotmail.com>
2025-07-26 13:19:19 +0300

    Get settings and booked days, importing getSettings amd getBookedDatesByCabinId functions, as Promise.all, and accept cabin passed from [cabinId] page

--- 21-the-wild-oasis-website/app/_components/Reservation.js
+++ 21-the-wild-oasis-website/app/_components/Reservation.js
@@ -1,7 +1,13 @@
 import DateSelector from "@/_components/DateSelector";
 import ReservationForm from "@/_components/ReservationForm";
+import { getBookedDatesByCabinId, getSettings } from "@/_lib/data-service";
 
-function Reservation() {
+async function Reservation({ cabin }) {
+  const [settings, bookedDays] = await Promise.all([
+    getSettings(),
+    getBookedDatesByCabinId(cabin.id),
+  ]);
+
   return (
     <div className="grid grid-cols-2 border border-primary-800 min-h-[400px]">
       <DateSelector />
