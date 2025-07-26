10fac3fbf513b94292f653977654fc53cbb9b9c4
deividas-butkus <deividas.butkus@hotmail.com>
2025-07-26 13:09:32 +0300

    Add comment "For testing" to setTimeout

--- 21-the-wild-oasis-website/app/_lib/data-service.js
+++ 21-the-wild-oasis-website/app/_lib/data-service.js
@@ -129,6 +129,7 @@ export async function getBookedDatesByCabinId(cabinId) {
 export async function getSettings() {
   const { data, error } = await supabase.from("settings").select("*").single();
 
+  // For testing
   await new Promise((res) => setTimeout(res, 2000));
 
   if (error) {
