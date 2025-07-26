be7c6169351a50d5e87c56030c8a647032e2bee3
deividas-butkus <deividas.butkus@hotmail.com>
2025-07-26 19:48:34 +0300

    Use ReservationReminder in cabins page

--- 21-the-wild-oasis-website/app/cabins/page.js
+++ 21-the-wild-oasis-website/app/cabins/page.js
@@ -1,5 +1,6 @@
 import CabinList from "@/_components/CabinList";
 import Filter from "@/_components/Filter";
+import ReservationReminder from "@/_components/ReservationReminder";
 import Spinner from "@/_components/Spinner";
 import { Suspense } from "react";
 
@@ -41,6 +42,7 @@ export default function Page({ searchParams }) {
 
       <Suspense fallback={<Spinner />} key={filter}>
         <CabinList filter={filter} />
+        <ReservationReminder />
       </Suspense>
     </div>
   );
