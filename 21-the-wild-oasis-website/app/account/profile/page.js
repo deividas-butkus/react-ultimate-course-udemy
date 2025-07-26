96380366767b09d8bc7d4ecf69b7f6d8c45302c0
deividas-butkus <deividas.butkus@hotmail.com>
2025-07-26 10:15:04 +0300

    Remove nationality and country flag placeholders from profile page

--- 21-the-wild-oasis-website/app/account/profile/page.js
+++ 21-the-wild-oasis-website/app/account/profile/page.js
@@ -3,10 +3,6 @@ export const metadata = {
 };
 
 export default function Page() {
-  // CHANGE
-  const countryFlag = "pt.jpg";
-  const nationality = "portugal";
-
   return (
     <div>
       <h2 className="font-semibold text-2xl text-accent-400 mb-4">
